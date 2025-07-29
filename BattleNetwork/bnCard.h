4e2c254fc56126f8c4f2051695ff5cf8a7059fe1
Alrysc <alrysc01@gmail.com>
2025-07-28 21:30:36 -0700

    Pull 52a8f9e, TF chips can be marked uncounterable
    Pull changes from 52a8f9e239ccaf345115b0b4b56cd13f4306ef03, which adds the "counterable" card property.

--- BattleNetwork/bnCard.h
+++ BattleNetwork/bnCard.h
@@ -37,6 +37,7 @@ namespace Battle {
       bool canBoost{ true }; /*!< Can this card be boosted by other cards? */
       bool timeFreeze{ false }; /*!< Does this card rely on action items to resolve before resuming the battle scene? */
       bool skipTimeFreezeIntro{ false }; /*! Skips the fade in/out and name appearing for this card */
+      bool counterable{ true };  /*!< During the tf intro, can this card be countered? */
       string shortname;
       string action; 
       string description;

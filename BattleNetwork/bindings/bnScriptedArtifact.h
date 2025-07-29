609196c12a09c038866889a76766d740ca4a15a1
Alrysc <alrysc01@gmail.com>
2025-07-29 01:03:47 -0700

    Pull 59d7262. Scripted Artifact, Obstacle, and Spell have battle start and end funcs.

--- BattleNetwork/bindings/bnScriptedArtifact.h
+++ BattleNetwork/bindings/bnScriptedArtifact.h
@@ -7,11 +7,11 @@
 #include "../bnAnimationComponent.h"
 #include "bnWeakWrapper.h"
 
-	/**
-	 * \class ScriptedArtifact
-	 * \brief An object in control of battlefield visual effects, with support for Lua scripting.
-	 * 
-	 */
+/**
+* \class ScriptedArtifact
+* \brief An object in control of battlefield visual effects, with support for Lua scripting.
+* 
+*/
 class ScriptedArtifact final : public Artifact, public dynamic_object
 {
 	std::shared_ptr<AnimationComponent> animationComponent{ nullptr };
@@ -20,29 +20,31 @@ class ScriptedArtifact final : public Artifact, public dynamic_object
 
 public:
 	ScriptedArtifact();
-	~ScriptedArtifact();
+  ~ScriptedArtifact();
 
-	void Init() override;
+  void Init() override;
 
-	/**
-	 * Centers the animation on the tile, offsets it by its internal offsets, then invokes the function assigned to onUpdate if present.
-	 * @param _elapsed: The amount of elapsed time since the last frame.
-	 */
-	void OnUpdate(double _elapsed) override;
-	void OnDelete() override;
-	bool CanMoveTo(Battle::Tile* next) override;
-	void OnSpawn(Battle::Tile& spawn) override;
+  /**
+   * Centers the animation on the tile, offsets it by its internal offsets, then invokes the function assigned to onUpdate if present.
+   * @param _elapsed: The amount of elapsed time since the last frame.
+   */
+  void OnUpdate(double _elapsed) override;
+  void OnDelete() override;
+  bool CanMoveTo(Battle::Tile* next) override;
+  void OnSpawn(Battle::Tile& spawn) override;
+  void OnBattleStart() override;
+  void OnBattleStop() override;
 
-	void SetAnimation(const std::string& path);
-	Animation& GetAnimationObject();
-	Battle::Tile* GetCurrentTile() const;
+  void SetAnimation(const std::string& path);
+  Animation& GetAnimationObject();
+  Battle::Tile* GetCurrentTile() const;
 
-	sol::object update_func;
-	sol::object on_spawn_func;
-	sol::object delete_func;
-	sol::object can_move_to_func;
-	sol::object battle_start_func;
-	sol::object battle_end_func;
+  sol::object update_func;
+  sol::object on_spawn_func;
+  sol::object delete_func;
+  sol::object can_move_to_func;
+  sol::object battle_start_func;
+  sol::object battle_end_func;
 };
 
 #endif
